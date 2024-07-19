6addafdd6e23cceaf47afc5c05f9e72f2b48edc6
Broche <xuym@360cbs.com>
2024-07-19 19:57:20 +0800

    数据统计

--- app/Http/Wow/Lib/XAutoAuctionLib.php
+++ app/Http/Wow/Lib/XAutoAuctionLib.php
@@ -15,15 +15,17 @@ class XAutoAuctionLib
     {
         $tempPath = $filePath . '.tmp' . date('Ymdhis');       // 创建一个临时文件路径
 
-        DB::connection('mysql' . $dbIndex)->beginTransaction();
+        $connection = DB::connection('mysql' . $dbIndex);
+        $connection->beginTransaction();
 
         $inFile = fopen($filePath, 'r');
         $outFile = fopen($tempPath, 'w');
 
-        $sellStr = '';
-        $buyStr = '';
-        $scanStr = '';
         if ($inFile && $outFile) {
+            $sellStr = '';
+            $buyStr = '';
+            $scanStr = '';
+
             while (($line = fgets($inFile)) !== false) {
                 if (!empty($res = self::processLineExport($line, self::XSellExportPrefix))) {
                     $sellStr = $res;
@@ -39,6 +41,65 @@ class XAutoAuctionLib
             rewind($inFile);
 
             // 处理数据
+            $summary = '导入摘要:';
+            if (!empty($scanStr)) {
+                $scanList = json_decode($scanStr);
+                $scanItems = [];
+                foreach ($scanList as $itemName => $item) {
+                    $dbItem = $connection->table('dat_item')->where('itemname', $itemName)->first();
+                    if (empty($dbItem)) {
+                        $connection->table('dat_item')->insert([
+                            'itemname' => $itemName,
+                            'vendorprice' => $item->vendorprice,
+                            'category' => $item->category,
+                            'class' => $item->class
+                        ]);
+                    } else {
+                        $connection->table('dat_item')->where('itemname', $itemName)->update([
+                            'vendorprice' => $item->vendorprice,
+                            'category' => $item->category,
+                            'class' => $item->class
+                        ]);
+                    }
+
+                    foreach ($item->list as $record) {
+                        $scanItems[] = [
+                            'itemname' => $itemName,
+                            'scantime' => $record->time,
+                            'price' => $record->price,
+                            'createtime' => time()
+                        ];
+                    }
+                }
+                if (!empty($scanItems)) {
+                    $connection->table('imp_scanhistory')->insert($scanItems);
+                    $summary .= '    扫描: ' . count($scanItems);
+                }
+            }
+            if (!empty($sellStr)) {
+                $sellList = json_decode($sellStr);
+                foreach ($sellList as $item) {
+                    $item->dealtime = $item->time;
+                    $item->dealdate = date('Y-m-d', $item->time);
+                    $item->createtime = time();
+                }
+                if (!empty($sellList)) {
+                    $connection->table('imp_sellhistory')->insert($sellList);
+                    $summary .= '    出售: ' . count($sellList);
+                }
+            }
+            if (!empty($buyStr)) {
+                $buyList = json_decode($buyStr);
+                foreach ($buyList as $item) {
+                    $item->buytime = $item->time;
+                    $item->buydate = date('Y-m-d', $item->time);
+                    $item->createtime = time();
+                }
+                if (!empty($buyList)) {
+                    $connection->table('imp_buyhistory')->insert($buyList);
+                    $summary .= '    购买: ' . count($buyList);
+                }
+            }
 
             $itemList = DB::connection('mysql' . $dbIndex)->table('dat_item')->get();
             $itemMap = json_decode(json_encode($itemList), true);
@@ -46,23 +107,15 @@ class XAutoAuctionLib
                 foreach ($item as $k => $v) {
                     $item[$k] = $v . '';
                 }
-                ksort($item);
             }
-
+            unset($item);
 
             if ($inFile && $outFile) {
                 while (($line = fgets($inFile)) !== false) {
                     self::processLineImport($line, self::XAuctionInfoListImportPrefix, $itemMap);
-
-                    if (!empty($res = self::processLineExport($line, self::XSellExportPrefix))) {
-                        $sellStr = $res;
-                    }
-                    if (!empty($res = self::processLineExport($line, self::XBuyExportPrefix))) {
-                        $buyStr = $res;
-                    }
-                    if (!empty($res = self::processLineExport($line, self::XScanExportPrefix))) {
-                        $scanStr = $res;
-                    }
+                    self::processLineExport($line, self::XSellExportPrefix);
+                    self::processLineExport($line, self::XBuyExportPrefix);
+                    self::processLineExport($line, self::XScanExportPrefix);
                     fwrite($outFile, $line);
                 }
             }
@@ -75,21 +128,9 @@ class XAutoAuctionLib
             unlink($filePath);
             rename($tempPath, $filePath);
         }
-        if (!empty($sellStr)) {
-            $sellList = json_decode($sellStr);
-            echo "selllist:\n";
-            echo json_encode($sellList, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        }
-        if (!empty($buyStr)) {
-            echo "buylist:\n";
-            $buyList = json_decode($buyStr);
-            echo json_encode($buyList, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        }
-        if (!empty($scanStr)) {
-            echo "scanlist:\n";
-            $scanList = json_decode($scanStr);
-            echo json_encode($scanList, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-        }
+        $connection->commit();
+
+        echo $summary;
     }
 
     private static function processLineImport(&$line, $prefix, $content)
@@ -119,4 +160,196 @@ class XAutoAuctionLib
         }
         return $result;
     }
+
+    private static function analyse($connection)
+    {
+        // scanprice, minscanprice, maxscanprice
+        $connection->update('update dat_item a inner join
+                                 (select itemname, avg(price) scanprice, min(price) minscanprice, max(price) maxscanprice
+                                  from imp_scanhistory
+                                  where price > 0
+                                  group by itemname) b on b.itemname = a.itemname
+                             set a.scanprice=b.scanprice, a.minscanprice=b.minscanprice, a.maxscanprice=b.maxscanprice');
+
+        // scanprice10, minscanprice10, maxscanprice10
+        $connection->update('update dat_item a inner join
+                                 (select itemname, avg(price) scanprice, min(price) minscanprice, max(price) maxscanprice
+                                  from imp_scanhistory
+                                  where price > 0
+                                    and scantime >= unix_timestamp() - 10 * 24 * 3600
+                                  group by itemname) b on b.itemname = a.itemname
+                             set a.scanprice10=b.scanprice, a.minscanprice10=b.minscanprice, a.maxscanprice10=b.maxscanprice');
+
+        // scanprice30, minscanprice30, maxscanprice30
+        $connection->update('update dat_item a inner join
+                                 (select itemname, avg(price) scanprice, min(price) minscanprice, max(price) maxscanprice
+                                  from imp_scanhistory
+                                  where price > 0
+                                    and scantime >= unix_timestamp() - 30 * 24 * 3600
+                                  group by itemname) b on b.itemname = a.itemname
+                             set a.scanprice30=b.scanprice, a.minscanprice30=b.minscanprice, a.maxscanprice30=b.maxscanprice');
+
+        // buyprice
+        $connection->update('update dat_item a inner join
+                                 (select itemname, sum(price*count)/sum(count) buyprice
+                                  from imp_buyhistory
+                                  where price > 0 and count > 0
+                                  group by itemname) b on b.itemname = a.itemname
+                             set a.buyprice=b.buyprice');
+
+        // buyprice10
+        $connection->update('update dat_item a inner join
+                                 (select itemname, sum(price*count)/sum(count) buyprice
+                                  from imp_buyhistory
+                                  where price > 0 and count > 0
+                                    and buytime >= unix_timestamp() - 10 * 24 * 3600
+                                  group by itemname) b on b.itemname = a.itemname
+                             set a.buyprice10=b.buyprice');
+
+        // buyprice30
+        $connection->update('update dat_item a inner join
+                                 (select itemname, sum(price*count)/sum(count) buyprice
+                                  from imp_buyhistory
+                                  where price > 0 and count > 0
+                                    and buytime >= unix_timestamp() - 30 * 24 * 3600
+                                  group by itemname) b on b.itemname = a.itemname
+                             set a.buyprice30=b.buyprice');
+
+        // default buyprice/10/30
+        $connection->update('update dat_item a
+                             set a.buyprice=if(a.buyprice = 0, a.scanprice, a.buyprice),
+                                 a.buyprice10=if(a.buyprice10 = 0, a.scanprice10, a.buyprice10),
+                                 a.buyprice30=if(a.buyprice30 = 0, a.scanprice30, a.buyprice30)');
+
+        // makeprice, makeprice10, makeprice30
+        $connection->update('update dat_item a inner join
+                                 (select z.itemname, max(price) price, max(price10) price10, max(price30) price30
+                                  from (select a.itemname,
+                                               sum(a.sourcecount * b.buyprice)   price,
+                                               sum(a.sourcecount * b.buyprice10) price10,
+                                               sum(a.sourcecount * b.buyprice30) price30
+                                        from dat_itemrecipe a
+                                                 inner join dat_item b on a.sourcename = b.itemname
+                                        group by a.itemname, a.type) z
+                                  group by z.itemname) z on a.itemname = z.itemname
+                             set a.makeprice=z.price,
+                                 a.makeprice10=z.price10,
+                                 a.makeprice30=z.price30');
+
+        // costprice, costprice10, costprice30
+        $connection->update('update dat_item a
+                             set a.costprice=if(a.buyprice > a.makeprice, a.buyprice, a.makeprice),
+                                 a.costprice10=if(a.buyprice10 > a.makeprice10, a.buyprice10, a.makeprice10),
+                                 a.costprice30=if(a.buyprice30 > a.makeprice30, a.buyprice30, a.makeprice30)');
+
+        // dealprice, dealcount
+        $connection->update('update dat_item a inner join
+                                 (select itemname, sum(price*count)/sum(count) dealprice, sum(count) dealcount
+                                  from imp_sellhistory
+                                  where price > 0 and count > 0
+                                  and issuccess = 1
+                                  group by itemname) b on b.itemname = a.itemname
+                             set a.dealprice=b.dealprice, a.dealcount=b.dealcount');
+
+        // dealprice10, dealcount10
+        $connection->update('update dat_item a inner join
+                                 (select itemname, sum(price*count)/sum(count) dealprice, sum(count) dealcount
+                                  from imp_sellhistory
+                                  where price > 0 and count > 0
+                                    and issuccess = 1
+                                    and dealtime >= unix_timestamp() - 10 * 24 * 3600
+                                  group by itemname) b on b.itemname = a.itemname
+                             set a.dealprice10=b.dealprice, a.dealcount10=b.dealcount');
+
+        // dealprice30, dealcount30
+        $connection->update('update dat_item a inner join
+                                 (select itemname, sum(price*count)/sum(count) dealprice, sum(count) dealcount
+                                  from imp_sellhistory
+                                  where price > 0 and count > 0
+                                    and issuccess = 1
+                                    and dealtime >= unix_timestamp() - 30 * 24 * 3600
+                                  group by itemname) b on b.itemname = a.itemname
+                             set a.dealprice30=b.dealprice, a.dealcount30=b.dealcount');
+
+        // sellcount
+        $connection->update('update dat_item a inner join
+                                 (select itemname, sum(count) sellcount
+                                  from imp_sellhistory
+                                  where count > 0
+                                  group by itemname) b on b.itemname = a.itemname
+                             set a.sellcount=b.sellcount');
+
+        // sellcount10
+        $connection->update('update dat_item a inner join
+                                 (select itemname, sum(count) sellcount
+                                  from imp_sellhistory
+                                  where count > 0
+                                    and dealtime >= unix_timestamp() - 10 * 24 * 3600
+                                  group by itemname) b on b.itemname = a.itemname
+                             set a.sellcount10=b.sellcount');
+
+        // sellcount30
+        $connection->update('update dat_item a inner join
+                                 (select itemname, sum(count) sellcount
+                                  from imp_sellhistory
+                                  where count > 0
+                                    and dealtime >= unix_timestamp() - 30 * 24 * 3600
+                                  group by itemname) b on b.itemname = a.itemname
+                             set a.sellcount30=b.sellcount');
+
+        // dealrate, dealrate10, dealrate30
+        $connection->update('update dat_item a
+                             set dealrate=if(dealcount = 0, 99, sellcount / dealcount),
+                                 dealrate10=if(dealcount10 = 0, 99, sellcount10 / dealcount10),
+                                 dealrate30=if(dealcount30 = 0, 99, sellcount30 / dealcount30)');
+
+        // lowestprice, lowestprice10, lowestprice30, profit, profit10, profit30
+        $connection->update('update dat_item a
+                             set a.lowestprice=if(a.dealrate = 0, 9999999, (a.costprice + a.vendorprice * 0.15 * a.dealrate) / 0.95),
+                                 a.lowestprice10=if(a.dealrate10 = 0, 9999999, (a.costprice10 + a.vendorprice * 0.15 * a.dealrate10) / 0.95),
+                                 a.lowestprice30=if(a.dealrate30 = 0, 9999999, (a.costprice30 + a.vendorprice * 0.15 * a.dealrate30) / 0.95),
+                                 a.profit=if(a.dealrate = 0, 0, (a.dealprice - a.costprice) / a.dealrate - a.vendorprice * 0.15 * (1 - 1 / a.dealrate)),
+                                 a.profit10=if(a.dealrate = 0, 0, (a.dealprice10 - a.costprice10) / a.dealrate10 - a.vendorprice * 0.15 * (1 - 1 / a.dealrate10)),
+                                 a.profit30=if(a.dealrate = 0, 0, (a.dealprice30 - a.costprice30) / a.dealrate30 - a.vendorprice * 0.15 * (1 - 1 / a.dealrate30))');
+
+        // profitrate, profitrate10, profitrate30
+        $connection->update('update dat_item a
+                             set a.profitrate=if(a.costprice = 0, 0, a.profit / a.costprice),
+                                 a.profitrate10=if(a.costprice10 = 0, 0, a.profit10 / a.costprice10),
+                                 a.profitrate30=if(a.costprice30 = 0, 0, a.profit30 / a.costprice30)');
+
+        // totalprofit, totalprofit10, totalprofit30, profitproportion, profitproportion10, profitproportion30
+        $connection->update('update dat_item a inner join
+                                 (select sum(profit * dealcount)     sumprofit,
+                                         sum(profit10 * dealcount10) sumprofit10,
+                                         sum(profit30 * dealcount30) sumprofit30
+                                  from dat_item) b
+                             set a.totalprofit=a.profit * a.dealcount,
+                                 a.totalprofit10=a.profit10 * a.dealcount10,
+                                 a.totalprofit30=a.profit30 * a.dealcount30,
+                                 a.profitproportion=if(b.sumprofit = 0, 0, a.profit * a.dealcount / b.sumprofit),
+                                 a.profitproportion10=if(b.sumprofit10 = 0, 0, a.profit10 * a.dealcount10 / b.sumprofit10),
+                                 a.profitproportion30=if(b.sumprofit30 = 0, 0, a.profit30 * a.dealcount30 / b.sumprofit30)');
+
+        // groupdealproportion, groupdealproportion10, groupdealproportion30, groupprofitproportion, groupprofitproportion10, groupprofitproportion30
+        $connection->update('update dat_item a left join
+                                 (select `group`,
+                                         sum(dealcount)              groupdealcount,
+                                         sum(dealcount10)            groupdealcount10,
+                                         sum(dealcount30)            groupdealcount30,
+                                         sum(profit * dealcount)     grouptotalprofit,
+                                         sum(profit10 * dealcount10) grouptotalprofit10,
+                                         sum(profit30 * dealcount30) grouptotalprofit30
+                                  from dat_item
+                                  where `group` <> \'\'
+                                  group by `group`) z on a.`group` = z.`group`
+                             set a.groupdealproportion=if(z.groupdealcount = 0, 0, ifnull(a.dealcount / z.groupdealcount, 0)),
+                                 a.groupdealproportion10=if(z.groupdealcount10 = 0, 0, ifnull(a.dealcount10 / z.groupdealcount10, 0)),
+                                 a.groupdealproportion30=if(z.groupdealcount30 = 0, 0, ifnull(a.dealcount30 / z.groupdealcount30, 0)),
+                                 a.groupprofitproportion=if(z.grouptotalprofit = 0, 0, ifnull(a.profit * a.dealcount, 0)),
+                                 a.groupprofitproportion10=if(z.grouptotalprofit10 = 0, 0, ifnull(a.profit10 * a.dealcount10, 0)),
+                                 a.groupprofitproportion30=if(z.grouptotalprofit30 = 0, 0, ifnull(a.profit30 * a.dealcount30, 0))');
+
+    }
+
 }
