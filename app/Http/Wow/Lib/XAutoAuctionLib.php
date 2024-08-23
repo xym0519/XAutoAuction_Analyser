c8df199fba8c22e5b7f1b55033aa0ab6beb3014a
Broche <xuym@360cbs.com>
2024-08-23 13:54:15 +0800

    优化，手续费计算

--- app/Http/Wow/Lib/XAutoAuctionLib.php
+++ app/Http/Wow/Lib/XAutoAuctionLib.php
@@ -404,10 +404,10 @@ class XAutoAuctionLib
         $connection->delete('delete from sta_dealjewcount');
 
         $connection->update("insert into sta_dealjewcount(日期, 星期, 收入, 成交, 
-                                出售, 成交率, 
+                                出售, 成交率, 手续, 
                                 赤玉, 紫黄, 王者, 祖尔, 巨锆, 恐惧, 血玉, 帝黄, 秋色, 森林, 天蓝, 曙光, 天焰, 大地)
                              select substr(y.d, 6) 日期, substr(y.w, 1,3) 星期, round(y.income/10000) 收入, y.success 成交,
-                                    y.total 出售, if(y.total=0, 0, round(y.success/y.total*100)) 成交率,
+                                    y.total 出售, if(y.total=0, 0, round(y.success/y.total*100)) 成交率, round(fee/10000) 手续,
                                     ifnull((select c from sta_dealcount a where a.dealdate=y.d and a.sourcename='赤玉石'), 0) 赤玉,
                                     ifnull((select c from sta_dealcount a where a.dealdate=y.d and a.sourcename='紫黄晶'), 0) 紫黄,
                                     ifnull((select c from sta_dealcount a where a.dealdate=y.d and a.sourcename='王者琥珀'), 0) 王者,
@@ -425,7 +425,8 @@ class XAutoAuctionLib
                              from(
                                  select z.d, z.w, ifnull(sum(b.price*b.count),0) income,
                                             ifnull(count(issuccess),0) total,
-                                            ifnull(sum(issuccess),0) success
+                                            ifnull(sum(issuccess),0) success,
+                                            ifnull(sum(if(issuccess=0, c.vendorprice*0.3, 0)),0) fee
                                      from (select from_unixtime(unix_timestamp()-day*24*3600, '%Y-%m-%d') d,
                                                   from_unixtime(unix_timestamp()-day*24*3600,'%W') w from dat_days) z
                                               left join imp_sellhistory b on dealdate = z.d
@@ -451,8 +452,8 @@ class XAutoAuctionLib
                                          + (select b.costprice10 from dat_item b where b.itemname = '大地侵攻钻石') * a.大地) / 10000)");
 
         $connection->update('update sta_dealjewcount
-                             set 利润=收入 - 成本,
-                                 利润率=(收入 - 成本) / 成本 * 100');
+                             set 利润=收入 - 成本 - 手续,
+                                 利润率=if(成本=0, 0, (收入 - 成本 - 手续) / 成本 * 100)');
 
         $connection->commit();
     }
